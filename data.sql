c5525b2c874849812c2b69a0bc683431e0d5f72b
Kinyua <alfkinyua@gmail.com>
2022-09-12 18:18:52 +0300

    Populate animals table with data

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_in_kg)values('Agumon', '2-3-2020', 0, true, 10.23);
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_in_kg)values('Gabumon', '11-5-2018', 2, true, 8);
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_in_kg)values('Pikachu', '1-7-2021', 1, false, 15.04);
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_in_kg)values('Devimon', '5-12-2017', 5, true, 11);;
+
